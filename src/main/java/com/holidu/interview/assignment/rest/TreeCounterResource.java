3d6c999ca7c438386b83463b4cd5234870c93ad7
kalviyaynen-km <java993@gmail.com>
2020-11-09 09:59:06 +0300

    added min value validation for radius param

--- src/main/java/com/holidu/interview/assignment/rest/TreeCounterResource.java
+++ src/main/java/com/holidu/interview/assignment/rest/TreeCounterResource.java
@@ -1,16 +1,18 @@
 package com.holidu.interview.assignment.rest;
 
-import com.holidu.interview.assignment.integration.soda2.Soda2IntegrationAdapter;
 import com.holidu.interview.assignment.service.TreeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Min;
 import java.util.*;
 
 @RestController
+@Validated
 public class TreeCounterResource {
     private static final Logger logger = LoggerFactory.getLogger(TreeCounterResource.class);
     private final TreeService treeService;
@@ -20,7 +22,7 @@ public class TreeCounterResource {
     }
 
     @GetMapping("/tree")
-    Map<String, Integer> trees(@RequestParam Double x, @RequestParam Double y, @RequestParam Long radius) {
+    Map<String, Integer> trees(@RequestParam Double x, @RequestParam Double y, @RequestParam @Min(1) Long radius) {
         long start = System.currentTimeMillis();
         Map<String, Integer> trees = treeService.getGroupedTrees(x, y, radius);
         logger.info("PROCESS TIME: " + (System.currentTimeMillis() - start) + " ms");
